8eaf17af88e116eab34681c360849815ac8a7e43
IngaRudakova <ingarudakova23@gmail.com>
2024-10-17 08:24:36 +0300

    17.10

--- lab2/dishes.js
+++ lab2/dishes.js
@@ -2,6 +2,7 @@ const dishes = [
     {
         keyword: 's_chicken', 
         name: "Суп с курицей", 
+        kind: "meat",
         price: 300, 
         category: 'soup', 
         count: '300 мл',
@@ -10,22 +11,52 @@ const dishes = [
     {
         keyword: 's_mushroom',
         name: "Суп с грибами",
+        kind: "veg",
         price: 250,
         category: 'soup', 
         count: '300 мл', 
         image: 'soup2.jpg'
     },
     {
+        keyword: 's_meat',
+        name: "Суп с фрикадельками",
+        kind: "meat",
+        price: 400,
+        category: 'soup', 
+        count: '450 мл', 
+        image: 'soup4.jpeg'
+    },
+    {
         keyword: 's_fish', 
         name: "Суп с рыбой", 
+        kind: "fish",
         price: 350,
         category: 'soup', 
         count: '250 мл', 
         image: 'soup3.jpg'
     },
     {
+        keyword: 's_grecha',
+        name: "Гречневый суп",
+        kind: "veg",
+        price: 250,
+        category: 'soup', 
+        count: '400 мл', 
+        image: 'soup5.jpeg'
+    },
+    {
+        keyword: 's_miso',
+        name: "Мисосуп",
+        kind: "fish",
+        price: 450,
+        category: 'soup', 
+        count: '300 мл', 
+        image: 'soup6.jpeg'
+    },
+    {
         keyword: 'chicken_potato', 
         name: "Курица с картошкой", 
+        kind: "meat",
         price: 300, 
         category: 'main', 
         count: '250 г',
@@ -34,6 +65,7 @@ const dishes = [
     {
         keyword: 'lazaniy', 
         name: "Лазанья", 
+        kind: "meat",
         price: 300, 
         category: 'main', 
         count: '250 г',
@@ -42,14 +74,160 @@ const dishes = [
     {
         keyword: 'rise_with_meat', 
         name: "Гуляш с рисом", 
+        kind: "meat",
         price: 400, 
         category: 'main', 
         count: '450 г',
         image: 'dish3.jpg'
     },
     {
+        keyword: 'fish_min', 
+        name: "Рыбное ассорти мини", 
+        kind: "fish",
+        price: 600, 
+        category: 'main', 
+        count: '650 г',
+        image: 'dish4.jpeg'
+    },
+    {
+        keyword: 'fish_max', 
+        name: "Рыбное ассорти макси", 
+        kind: "fish",
+        price: 900, 
+        category: 'main', 
+        count: '1050 г',
+        image: 'dish5.jpeg'
+    },
+    {
+        keyword: 'mushroom_pasta', 
+        name: "Греческая паста с грибами", 
+        kind: "veg",
+        price: 500, 
+        category: 'main', 
+        count: '600 г',
+        image: 'dish6.jpeg'
+    },
+    {
+        keyword: 'veget_pasta', 
+        name: "Греческая паста с овощами", 
+        kind: "veg",
+        price: 400, 
+        category: 'main', 
+        count: '600 г',
+        image: 'dish7.jpeg'
+    },
+    {
+        keyword: 'salat_rukola', 
+        name: "Салат с рукколой", 
+        kind: "veg",
+        price: 300, 
+        category: 'salat', 
+        count: '400 г',
+        image: 'salat1.jpeg'
+    },
+    {
+        keyword: 'salat_imbir', 
+        name: "Салат с имберем", 
+        kind: "veg",
+        price: 350, 
+        category: 'salat', 
+        count: '400 г',
+        image: 'salat2.jpeg'
+    },
+    {
+        keyword: 'salat_gov', 
+        name: "Салат с говядиной", 
+        kind: "meat",
+        price: 400, 
+        category: 'salat', 
+        count: '300 г',
+        image: 'salat3.jpeg'
+    },
+    {
+        keyword: 'salat_indeika', 
+        name: "Салат с индейкой", 
+        kind: "meat",
+        price: 350, 
+        category: 'salat', 
+        count: '250 г',
+        image: 'salat4.jpeg'
+    },
+    {
+        keyword: 'salat_tun', 
+        name: "Салат с тунцом", 
+        kind: "fish",
+        price: 400, 
+        category: 'salat', 
+        count: '250 г',
+        image: 'salat5.jpeg'
+    },
+    {
+        keyword: 'salat_krevetka', 
+        name: "Салат с креветками", 
+        kind: "fish",
+        price: 500, 
+        category: 'salat', 
+        count: '350 г',
+        image: 'salat6.jpeg'
+    },
+    {
+        keyword: 'napoleon', 
+        name: "Наполеон", 
+        kind: "medium",
+        price: 300, 
+        category: 'desert', 
+        count: '150 г',
+        image: 'desert1.jpeg'
+    },
+    {
+        keyword: 'ice_cream_min', 
+        name: "Шоколадное мороженое", 
+        kind: "medium",
+        price: 200, 
+        category: 'desert', 
+        count: '90 г',
+        image: 'desert2.jpeg'
+    },
+    {
+        keyword: 'ice_cream_max', 
+        name: "Snikers мороженое", 
+        kind: "large",
+        price: 400, 
+        category: 'desert', 
+        count: '300 г',
+        image: 'desert3.jpeg'
+    },
+    {
+        keyword: 'panakota', 
+        name: "Панакота с орехами", 
+        kind: "large",
+        price: 350, 
+        category: 'desert', 
+        count: '250 г',
+        image: 'desert4.jpeg'
+    },
+    {
+        keyword: 'ulitka', 
+        name: "Улитка с изюмом", 
+        kind: "small",
+        price: 250, 
+        category: 'desert', 
+        count: '60 г',
+        image: 'desert5.jpeg'
+    },
+    {
+        keyword: 'halva', 
+        name: "Халва", 
+        kind: "small",
+        price: 150, 
+        category: 'desert', 
+        count: '50 г',
+        image: 'desert6.jpeg'
+    },
+    {
         keyword: 'fruit_drink', 
         name: "Компот", 
+        kind: "cold",
         price: 80, 
         category: 'drink', 
         count: '400 мл',
@@ -58,6 +236,7 @@ const dishes = [
     {
         keyword: 'smuzi', 
         name: "Смузи", 
+        kind: "cold",
         price: 250, 
         category: 'drink', 
         count: '250 мл',
@@ -66,11 +245,67 @@ const dishes = [
     {
         keyword: 'carrot_drink', 
         name: "Морковный сок", 
+        kind: "cold",
         price: 200, 
         category: 'drink', 
         count: '300 мл',
         image: 'drink3.jpg'
+    },
+    {
+        keyword: 'fruit_drink_apple', 
+        name: "Компот яблочный", 
+        kind: "cold",
+        price: 90, 
+        category: 'drink', 
+        count: '400 мл',
+        image: 'drink4.jpeg'
+    },
+    {
+        keyword: 'smuzi_banan', 
+        name: "Смузи на молоке с бананом", 
+        kind: "cold",
+        price: 350, 
+        category: 'drink', 
+        count: '250 мл',
+        image: 'drink5.jpeg'
+    },
+    {
+        keyword: 'sprite', 
+        name: "Спрайт", 
+        kind: "cold",
+        price: 250, 
+        category: 'drink', 
+        count: '500 мл',
+        image: 'drink6.jpeg'
+    },
+    {
+        keyword: 'americano', 
+        name: "Американо", 
+        kind: "hot",
+        price: 200, 
+        category: 'drink', 
+        count: '100 мл',
+        image: 'drink7.jpeg'
+    },
+    {
+        keyword: 'mokko', 
+        name: "Мокко", 
+        kind: "hot",
+        price: 200, 
+        category: 'drink', 
+        count: '200 мл',
+        image: 'drink8.jpeg'
+    },
+    {
+        keyword: 'tea', 
+        name: "Чай", 
+        kind: "hot",
+        price: 150, 
+        category: 'drink', 
+        count: '300 мл',
+        image: 'drink9.jpeg'
     }
+
     
 ];
 
--- lab2/displayDishes.js
+++ lab2/displayDishes.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const soupSection = document.querySelector("#soup-section .dishes");
     const mainSection = document.querySelector("#main-section .dishes");
+    const salatSection = document.querySelector("#salat-section .dishes");
+    const desertSection = document.querySelector("#desert-section .dishes");
     const drinkSection = document.querySelector("#drink-section .dishes");
 
     dishes.sort((a, b) => a.name.localeCompare(b.name));
@@ -14,8 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
         dishElement.innerHTML = ` 
             <img src="${dish.image}" alt="${dish.name}">
             <div class="dish-info">
-                <p class="price">${dish.price}₽</p>
+                <p class="kind">${dish.kind}</p>
                 <p class="name">${dish.name}</p>
+                <p class="price">${dish.price}₽</p>
                 <p class="weight">${dish.count}</p>
                 <button class="add">Добавить</button>
             </div>
@@ -25,6 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
             soupSection.appendChild(dishElement);
         } else if (dish.category === "main") {
             mainSection.appendChild(dishElement);
+        } else if (dish.category === "salat") {
+            salatSection.appendChild(dishElement);
+        } else if (dish.category === "desert") {
+            desertSection.appendChild(dishElement);   
         } else if (dish.category === "drink") {
             drinkSection.appendChild(dishElement);
         }
@@ -34,6 +41,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const orderForm = {
         soup: document.getElementById("selected-soup"),
         main: document.getElementById("selected-main-dish"),
+        salat: document.getElementById("selected-salat"),
+        desert: document.getElementById("selected-desert"),
         drink: document.getElementById("selected-drink"),
         totalPrice: document.querySelector("#total-price .price-value")
     };
@@ -41,6 +50,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let selectedDishes = {
         soup: null,
         main: null,
+        salat: null,
+        desert: null,
         drink: null
     };
     const updateOrder = () => {
@@ -60,6 +71,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const noSelectionMessage = document.getElementById("no-selection-message");
         const soupOrder = document.getElementById("soup-order");
         const mainOrder = document.getElementById("main-order");
+        const salatOrder = document.getElementById("salat-order");
+        const desertOrder = document.getElementById("desert-order");
         const drinkOrder = document.getElementById("drink-order");
         const totalPriceBlock = document.getElementById("total-price-block");
 
@@ -68,12 +81,16 @@ document.addEventListener("DOMContentLoaded", () => {
             noSelectionMessage.style.display = 'block';
             soupOrder.style.display = 'none';
             mainOrder.style.display = 'none';
+            salatOrder.style.display = 'none';
+            desertOrder.style.display = 'none';
             drinkOrder.style.display = 'none';
             totalPriceBlock.style.display = 'none';
         } else {
             noSelectionMessage.style.display = 'none';
             soupOrder.style.display = 'block';
             mainOrder.style.display = 'block';
+            salatOrder.style.display = 'block';
+            desertOrder.style.display = 'block';
             drinkOrder.style.display = 'block';
             totalPriceBlock.style.display = 'block';
             orderForm.totalPrice.textContent = `${total}₽`;
